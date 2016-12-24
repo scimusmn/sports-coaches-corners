31aee368ec5e8bfb7f1a51e5c02e5067de99d518
bryan kennedy <bkennedy@smm.org>
2016-12-24 13:32:30 -0600

    Add the component number to the component list

--- imports/ui/components/ExhibitComponentsList.js
+++ imports/ui/components/ExhibitComponentsList.js
@@ -3,8 +3,12 @@ import { ListGroup, ListGroupItem, Alert } from 'react-bootstrap';
 
 const ExhibitComponentsList = ({ exhibitComponents }) => (
   exhibitComponents.length > 0 ? <ListGroup className="DocumentsList">
-    {exhibitComponents.map(({ _id, title }) => (
-      <ListGroupItem key={ _id } href={`/components/${_id}`}>{ title }</ListGroupItem>
+    {exhibitComponents.map(({ _id, title, componentNumber}) => (
+      <ListGroupItem
+        key={ _id }
+        href={`/components/${_id}`}>
+        { componentNumber } - { title }
+      </ListGroupItem>
     ))}
   </ListGroup> :
   <Alert bsStyle="warning">No exhibit components yet.</Alert>
