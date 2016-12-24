d5ea004fb7f518a6ba946521d201e6ed991fb65c
bryan kennedy <bkennedy@smm.org>
2016-12-24 15:20:43 -0600

    Fixing video publication
    Typo and including all fields

--- imports/api/videos/server/publications.js
+++ imports/api/videos/server/publications.js
@@ -2,10 +2,12 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import Videos from '../videos';
 
-Meteor.publish('video.list', () => Videos.find());
+Meteor.publish('videos.list', () => Videos.find());
 
-Meteor.publish('video.view', (componentNumber, videoNum) => {
+Meteor.publish('video.view', (componentNumber, questionEn, questionEs, videoNum) => {
   check(componentNumber, String);
+  check(questionEn, Number);
+  check(questionEs, String);
   check(videoNum, Number);
   return Videos.find({ $and: [{ componentNumber }, { videoNum }] });
 });
