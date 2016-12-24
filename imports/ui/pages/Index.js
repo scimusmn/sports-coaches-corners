4053d2ea55d26eab0f2f7c4c56b7aa019acfb905
bryan kennedy <bkennedy@smm.org>
2016-12-24 13:55:43 -0600

    Customizing boilerplate to coaches corners

--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -4,10 +4,8 @@ import { Jumbotron } from 'react-bootstrap';
 const Index = () => (
   <div className="Index">
     <Jumbotron className="text-center">
-      <h2>Base</h2>
-      <p>A starting point for Meteor applications.</p>
-      <p><a className="btn btn-success" href="https://themeteorchef.com/base" role="button">Read the Documentation</a></p>
-      <p style={ { fontSize: '16px', color: '#aaa' } }>Currently at v4.10.0</p>
+      <h2>Sports - Coaches Corners</h2>
+      <p>Application for displaying and editing Coaches Corners kiosks in the Sportsology exhibit.</p>
     </Jumbotron>
   </div>
 );
