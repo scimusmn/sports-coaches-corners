bdae9e32953f08b6dad6f95e45d194dc6e1dbabf
bryan kennedy <bkennedy@smm.org>
2016-12-26 10:44:29 -0600

    Style fix

--- imports/ui/components/ExhibitComponentsList.js
+++ imports/ui/components/ExhibitComponentsList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import '/node_modules/react-bootstrap-table/dist/react-bootstrap-table.min.css';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
 
 class ExhibitComponentsList extends React.Component {
   constructor(props) {
