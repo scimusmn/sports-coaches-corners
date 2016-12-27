34af444d03e1d162bcaf6b8d06268986bbf4be60
bryan kennedy <bkennedy@smm.org>
2016-12-27 15:06:59 -0600

    Removing unused props

--- imports/ui/components/VideoPlayer.js
+++ imports/ui/components/VideoPlayer.js
@@ -3,15 +3,7 @@ import VideoHomeButton from './VideoHomeButton';
 
 class VideoPlayer extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      video: props.video,
-    };
-  }
-
   render() {
-    const video = this.props.video;
     const selectedVideo = () => this.props.selectedVideo.replace('video-', '');
 
     return (
@@ -35,7 +27,6 @@ class VideoPlayer extends React.Component {
 }
 
 VideoPlayer.propTypes = {
-  video: React.PropTypes.object,
   handleHomeAction: React.PropTypes.func,
   componentNumber: React.PropTypes.string,
   selectedVideo: React.PropTypes.string,
